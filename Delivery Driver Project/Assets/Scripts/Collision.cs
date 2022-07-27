e064bd96080f6697d05c784406c21db80762ac03
Steven Tran <thisissteventranwow@gmail.com>
2022-07-26 23:06:55 -0700

    Assets Added

--- Delivery Driver Project/Assets/Scripts/Collision.cs
+++ Delivery Driver Project/Assets/Scripts/Collision.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    private void OnCollisionEnter2D(Collision2D other) {
+
+        Debug.Log("Collision");
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger Collision");
     }
+
+
 }
+
+
